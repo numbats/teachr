7c603e75332a1e53d0fd8faa800a452993f1d6a1
mitchelloharawild <mitch.ohara-wild@monash.edu>
2022-02-02 11:46:44 +1100

    Re-organise helpers and + docs

--- R/chapter.R
+++ R/chapter.R
@@ -1,11 +1,8 @@
 
-#' R Markdown format for Distill articles
+#' R Markdown format for course-starter chapter
 #'
-#' Scientific and technical writing, native to the web.
-#'
-#' Distill articles feature attractive, reader-friendly typography, flexible
-#' layout options for visualizations, and full support for footnotes and
-#' citations.
+#' Generates the md output suitable for producing chapters in the
+#' ines/course-starter template.
 #'
 #' @inheritParams rmarkdown::html_document
 #'
@@ -54,66 +51,4 @@ teachr_chapter <- function(self_contained = TRUE,
       ...
     )
   )
-}
-
-
-#' Slide output for the chapter
-#'
-#' @param name The name of the slide without the file extension
-#' @export
-slides <- function(name) {
-  if(knitr::is_html_output(excludes = c("markdown"))) {
-    glue::glue("<iframe src='../slides/{name}.html' width='800' height='500'></iframe>")
-  } else {
-    glue::glue('<slides source="{name}">\n</slides>')
-  }
-}
-
-#' Mulitple choice options for the chapter
-#' @param ... A name-value pair where name shows the text, value shows the
-#'   message. Each pair should be an option.
-#' @param correct The number of the option that is correct.
-#' @export
-mc_opts <- function(..., correct = NULL) {
-  messages <- list(...)
-  text <- names(messages)
-  if(knitr::is_html_output(excludes = c("markdown"))) {
-    out <- '<div>'
-    for(i in seq_along(messages)) {
-      answer <- ifelse(i==correct, '<span style="color:red">CORRECT</span>', '')
-      out <- c(out, glue::glue('<input type="radio"> {text[i]} | {answer} {messages[i]}'))
-    }
-    out <- c(out, "</div>")
-  } else {
-    out <- '<choice>'
-    for(i in seq_along(messages)) {
-      answer <- ifelse(i==correct, 'correct="true"', '')
-      out <- c(out, glue::glue('<opt text="{text[i]}" {answer}>'), messages[i], '</opt>')
-    }
-    out <- c(out, "</choice>")
-  }
-  paste(out, collapse = "\n\n")
-}
-
-
-#' The codeblock for the chapter
-#' @param id The id of the codeblock
-#' @param hint The hint.
-#' @export
-codeblock <- function(id, hint) {
-  if(knitr::is_html_output(excludes = c("markdown"))) {
-    out <- c('<pre class="r">',
-             paste("<code>",
-                   readLines(sprintf("../exercises/exc_%s.R", id)),
-                   "</code>"),
-             '</pre>',
-             '<details open><summary>Hint</summary>',
-             hint,
-             '</details>')
-  } else {
-    out <- c(glue::glue('<codeblock id="{id}">'),
-             hint,
-             "</codeblock>")
-  }
-  paste(out, collapse = "\n")
 }
