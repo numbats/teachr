cfdaf6ce0a71e1ab46ef801eaedf18b8f80a8c27
mitchelloharawild <mitch.ohara-wild@monash.edu>
2022-02-25 21:11:38 +1100

    Rework expressions for learnr chunk

--- R/knitr_engine.R
+++ R/knitr_engine.R
@@ -1,14 +1,13 @@
 teachr_engine <- function(options) {
+  label <- knitr::opts_current$get()$label
   code <- options$code
-  outdir <- options$outdir %||% "exercises"
+  outdir <- options$outdir %||% "../exercises"
 
   testthat_sep <- cumsum(grepl("^\\?\\?\\?\\s*", code))
   if(testthat_sep[length(testthat_sep)] > 1) {
     stop("Only one testthat section can be provided in each teachr chunk.")
   }
   code <- split(code, c("code", "test")[testthat_sep+1])
-  code$test[1] <- "library(testthat)"
-
   hint_sep <- cumsum(grepl("^---\\s*", code$code))
   if(hint_sep[length(hint_sep)] > 1) {
     stop("Only one hint can be provided in each teachr chunk.")
@@ -16,11 +15,18 @@ teachr_engine <- function(options) {
   code[c("code", "hint")] <- split(code$code, factor(c("code", "hint"))[hint_sep+1])
   if(length(code$hint) > 0) code$hint <- code$hint[-1]
 
+  # Add default starter code substitutes
+  code$code <- gsub(
+    paste0(options[[".open"]]%||%"\\{<","([^🎯]+)",options[[".close"]]%||%">\\}"),
+    paste0(options[[".open"]]%||%"\\{<","\\1🎯___",options[[".close"]]%||%">\\}"),
+    code$code
+  )
+
   # Separate solutions from starter
   solution_patterns <- list(
-    inline = paste0(options[[".open"]]%||%"<<","(.+?)",options[[".close"]]%||%">>")
+    inline = paste0(options[[".open"]]%||%"\\{<","([^🎯]+)🎯?(.*?)",options[[".close"]]%||%">\\}")
   )
-  starter <- gsub(solution_patterns$inline, "_____", code$code)
+  starter <- gsub(solution_patterns$inline, "\\2", code$code)
   solution <- gsub(solution_patterns$inline, "\\1", code$code)
 
   # If ran outside of a knit, run the solution code.
@@ -29,24 +35,29 @@ teachr_engine <- function(options) {
     library(testthat)
 
     options$code <- solution
+    options$cache <- FALSE
     # Run R code as usual
     out <- knitr::knit_engines$get("R")(options)
 
-    # Run tests on R code
-    res <- capture.output(
-      eval(call("test_that", "teachr", str2expression(code$test)))
+    code$test <- c(
+      "suppressPackageStartupMessages(library(testthat))",
+      paste0("test_that(\"", label, "\", {"),
+      code$test[-1],
+      "})"
     )
+
+    # Run tests on R code
+    res <- capture.output(eval(str2expression(code$test)))
 
     # Return combined result
     return(paste0(c(out, "", res), collapse = "\n"))
   }
 
   # Otherwise, create exercise files for the course website
-  label <- knitr::opts_current$get()$label
   xfun::dir_create(outdir)
   xfun::write_utf8(starter, file.path(outdir, paste0("exc_", label, ".R")))
   xfun::write_utf8(solution, file.path(outdir, paste0("solution_", label, ".R")))
-  xfun::write_utf8(code$test, file.path(outdir, paste0("test_", label, ".R")))
+  xfun::write_utf8(code$test[-1], file.path(outdir, paste0("test_", label, ".R")))
   knitr::asis_output(
     paste0(
       '<codeblock id="', label, '">\n',
